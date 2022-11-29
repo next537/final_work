e4e4ac9419c30e941788e0f720cc12082b6e1a57
next537 <45046731+next537@users.noreply.github.com>
2022-11-29 17:23:01 +0300

    Add program

---
+++ work/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] { "1767", "352", "789", "world", "hello" };
+string[] array2 = new string[array1.Length];
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array1, array2);
+PrintArray(array2);
